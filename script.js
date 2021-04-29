139835a81dea5b51ae7724582cbff9ed0c469d68
shota-mamiya <shota.mamiya@outlook.com>
2021-04-29 14:27:14 -0700

    Dojo Weather

---
+++ script.js
@@ -0,0 +1,9 @@
+function hideCookies(){
+    let element = document.getElementById("cookiebanner");
+    element.style.visibility = ("hidden");
+}
+
+function alertBox(){
+    alert("Loading Weather Report...")
+}
+
